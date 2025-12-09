de122965c15a2f02d8a45467b2966d34b7d9386d
Robani Diansyah <melinting522@gmail.com>
2025-12-09 15:35:37 +0700

    refactor: ganti struktur default nest dengan ChatbotModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatbotModule } from './chatbot/chatbot.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ChatbotModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
