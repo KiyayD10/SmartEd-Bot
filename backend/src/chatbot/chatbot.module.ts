444e7717d90dfee64908fbd46b906aa865bedcc1
Robani Diansyah <melinting522@gmail.com>
2025-12-09 15:52:30 +0700

    feat: daftarkan controller dan service ke dalam module

--- backend/src/chatbot/chatbot.module.ts
+++ backend/src/chatbot/chatbot.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatbotController } from './chatbot.controller';
+import { ChatbotService } from './chatbot.service';
 
-@Module({})
+@Module({
+  controllers: [ChatbotController],
+  providers: [ChatbotService],
+})
 export class ChatbotModule {}
