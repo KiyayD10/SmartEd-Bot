dfb88890fa564cc2a2505295d04922fb178b2fe7
Robani Diansyah <melinting522@gmail.com>
2025-12-11 16:34:58 +0700

    fix: matiin warning hydration error gara-gara extension browser

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
